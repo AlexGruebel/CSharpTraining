d06232749c09298e30c73d68e8e92f9a541288c8
AlexGruebel <alexander-gruembel@online.de>
2018-03-27 11:44:59 +0200

    Working with NestedAndChildTasks

---
+++ Chapter13/NestedAndChildTasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace NestedAndChildTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outer = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Outer task starting ...");
+                var inner = Task.Factory.StartNew(()=>{
+                    Console.WriteLine("Inner Task starting ...");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Inner task finished.");
+                }, TaskCreationOptions.AttachedToParent);
+            }
+            );
+
+            outer.Wait();
+            Console.WriteLine("Outer task finished.");
+            Console.ReadLine();
+        }
+    }
+}

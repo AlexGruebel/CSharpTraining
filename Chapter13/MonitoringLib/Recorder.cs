16ef7e499f70638b27fd1ee660b6eb5b6f674a94
AlexGruebel <alexander-gruembel@online.de>
2018-03-27 11:29:20 +0200

    Chapter 13,  monitoring performence and working with taks

---
+++ Chapter13/MonitoringLib/Recorder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+namespace MonitoringLib
+{
+    public static class Recorder
+    {
+        static Stopwatch timer = new Stopwatch();
+        static long bytesPhysicalBefore = 0;
+        static long bytesVirtualBefore = 0;
+
+        public static void Start()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            bytesPhysicalBefore = Process.GetCurrentProcess().WorkingSet64;
+            bytesVirtualBefore = Process.GetCurrentProcess().VirtualMemorySize64;
+            timer.Restart();
+        }
+
+        public static void Stop()
+        {
+            timer.Stop();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            long bytesPhysicalAfter = Process.GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = Process.GetCurrentProcess().VirtualMemorySize64;
+            WriteLine("Stopped recording");
+            WriteLine($"{bytesPhysicalAfter - bytesPhysicalBefore} pyscial bytes used");
+            WriteLine($"{bytesVirtualAfter - bytesVirtualBefore} virtual bytes used");
+
+            WriteLine($"{timer.Elapsed} time span ellapsed");
+            WriteLine($"{timer.ElapsedMilliseconds} total millisecounds ellapsed");
+        }
+
+    }
+}

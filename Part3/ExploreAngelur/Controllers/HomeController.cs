7d56563593b9ae977b7185b86bed0e5fd76e37ed
AlexGruebel <alexander-gruembel@online.de>
2018-07-18 22:00:25 +0200

    regenerated project ExploreAngular

--- Part3/ExploreAngelur/Controllers/HomeController.cs
+++ Part3/ExploreAngelur/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ExploreAngular.Controllers
+namespace ExploreAngelur.Controllers
 {
     public class HomeController : Controller
     {
--- Part3/ExploreAngelur/Startup.cs
+++ Part3/ExploreAngelur/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ExploreAngular
+namespace ExploreAngelur
 {
     public class Startup
     {
